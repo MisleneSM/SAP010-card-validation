0b06a22e43b14e8e4b1f7b758ba3b5b4e9fea16b
Mislene Silva Moura <mislene.moura2000@gmail.com>
2023-04-25 23:05:04 -0300

    Alterações de documento README, alterações do HTML e CSS, arquivos dos comandos no JavaScript index.js e validator.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import validator from './validator.js';
 
-console.log(validator);
+//Dados dos imputs e botoes//
+const numeroCartao = document.querySelector('#cartaoNumber');
+const botao = document.querySelector("#validar");
+const numCartao = document.querySelector("#numCartao");
+const digCartao = document.querySelector('.digitos');
 
-const numCartao = document.getElementById("name");
-const botao = document.getElementById("validar");
+numeroCartao.oninput = function (e) { //função oninput puxa o valor do input
+  digCartao.innerText = validator.maskify(e.target.value); //mostra o valor do imput no cartão rosa
+}
+
+botao.oninput = function (e) {
+  numCartao.innerText = validator.isValid(e.target.value);
+}
+const cartaoValidar = numeroCartao.value;
+const validarNumber = validator.isValid(cartaoValidar);
+const resulted = validarNumber;
+if (resulted === true){ 
+  alert("Seu cartão foi validado, por favor aguarde")
+} else {
+  alert("Seu cartão é inválido, tente novamente")
+}
